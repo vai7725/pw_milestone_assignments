681b38a5fcf1f75a23429a63bd055c9979e25884
Vaibhav Prajapat <vaibhav.prajapat.7725@gmail.com>
2023-03-14 22:07:44 +0530

    vowel counter bug fixed

--- milestone 1/js/vowelCounter.js
+++ milestone 1/js/vowelCounter.js
@@ -1,22 +1,22 @@
-const string = document.getElementById("str");
-const btn = document.querySelector(".btn");
-const msg = document.querySelector(".msg");
+const string = document.getElementById('str');
+const btn = document.querySelector('.btn');
+const msg = document.querySelector('.msg');
 
-btn.addEventListener("click", (e) => {
+btn.addEventListener('click', (e) => {
   e.preventDefault();
-  const str = string.value;
+  const str = string.value.toLowerCase();
   console.log(str);
   let count = 0;
   for (let i = 0; i < str.length; i++) {
     if (
-      str[i] == "a" ||
-      str[i] == "e" ||
-      str[i] == "i" ||
-      str[i] == "o" ||
-      str[i] == "au"
+      str[i] == 'a' ||
+      str[i] == 'e' ||
+      str[i] == 'i' ||
+      str[i] == 'o' ||
+      str[i] == 'u'
     ) {
       count += 1;
     }
   }
-  msg.innerText = "No. of vowels: " + count;
+  msg.innerText = 'No. of vowels: ' + count;
 });
