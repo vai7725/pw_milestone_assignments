b95c4268fdc4750a3b28f5b275cdc7263ca95787
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-07-25 16:39:40 +0530

    Minor bug fixed

--- crud assignment/model/user.js
+++ crud assignment/model/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const user = new mongoose.Schema({
   email: {
     type: String,
+    unique: true,
     required: [true, 'Email is required.'],
   },
   password: {
